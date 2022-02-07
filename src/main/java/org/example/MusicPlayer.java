1bf970d057e9fba63a1dcbccc27aa49b60d4ef52
thgdanilaya <frederigo234@outlook.com>
2022-02-07 13:37:30 +0300

    DI with setter by value

--- src/main/java/org/example/MusicPlayer.java
+++ src/main/java/org/example/MusicPlayer.java
@@ -3,16 +3,38 @@ package org.example;
 public class MusicPlayer {
     private Music music;
 
+    private String name;
+    private int value;
+
     //IoC
     public MusicPlayer(Music music) {
         this.music = music;
     }
-    public MusicPlayer() {}
+
+    public MusicPlayer() {
+    }
+
     public void setMusic(Music music) {
         this.music = music;
     }
 
     public void PlayMusic() {
         System.out.println("Playing: " + music.getSong());
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 }
--- src/main/java/org/example/TestSpring.java
+++ src/main/java/org/example/TestSpring.java
@@ -9,6 +9,8 @@ public class TestSpring {
         );
         MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
         musicPlayer.PlayMusic();
+        System.out.println(musicPlayer.getName());
+        System.out.println(musicPlayer.getValue());
         context.close();
     }
 }
